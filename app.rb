4a4c643204938d0c32cad7cf2148b85458ef9d67
bahayg <bahayg@gmail.com>
2019-11-19 13:21:06 -0800

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+
+        page.have_selector(form)
+    end
+
+    post '/team' do
+        
+    en
 
 end
